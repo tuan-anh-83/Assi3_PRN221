95b253b33ac470dcf831a2f5d111a36910656b6a
tuan-anh-83 <117378521+tuan-anh-83@users.noreply.github.com>
2024-03-12 16:01:36 +0700

    Add project files.

---
+++ Assi3_PRN221/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace Assi3_PRN221
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int countdownTime;
+        private Thread countdownThread;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartCountdown_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(txtCountdownTime.Text, out int time))
+            {
+                countdownTime = time;
+                countdownThread = new Thread(new ThreadStart(Countdown));
+                countdownThread.Start();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid integer for countdown time.");
+            }
+        }
+
+        private void Countdown()
+        {
+            for (int i = countdownTime; i >= 0; i--)
+            {
+                UpdateCountdownDisplay(i);
+                Thread.Sleep(1000); // Sleep for 1 second
+            }
+        }
+
+        private void UpdateCountdownDisplay(int secondsRemaining)
+        {
+            if (txtCountdownDisplay.Dispatcher.CheckAccess())
+            {
+                txtCountdownDisplay.Text = TimeSpan.FromSeconds(secondsRemaining).ToString(@"hh\:mm\:ss");
+            }
+            else
+            {
+                txtCountdownDisplay.Dispatcher.Invoke(() => UpdateCountdownDisplay(secondsRemaining));
+            }
+        }
+    }
+}
